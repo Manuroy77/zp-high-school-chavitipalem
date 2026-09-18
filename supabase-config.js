3ec53a56eca6c73525d353b40f867254c00378db
Manuroy.github.io <manumoparthi07@gmail.com>
2026-09-19 00:41:15 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://bxawygomrmpsofyhloms.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_XgJymTA3ejmoKaIQGEAhDA_O6ociqh0";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
